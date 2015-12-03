8841fa3c34b324adedb0854d687ae482d2df66d4
Shawn Jain <jains@mit.edu>
2015-12-03 01:39:11 +0000

    vsl file added; not done

---
+++ secfs/vsl.py
@@ -0,0 +1,11 @@
+
+
+class VS:
+    def __init__(self):
+        # user's i-handle
+        self.ihandle = None
+        # list of some group i-handles
+        self.group_ihandles = []
+        # user's version vector
+        self.version_vector = {}
+
